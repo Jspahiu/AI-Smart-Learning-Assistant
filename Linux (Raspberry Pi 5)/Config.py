0a15b77f3e76cdc50b95634948a4783e800275a4
Jspahiu <121984809+Jspahiu@users.noreply.github.com>
2025-07-20 14:42:08 -0500

    Update Config.py

--- Linux (Raspberry Pi 5)/Config.py
+++ Linux (Raspberry Pi 5)/Config.py
@@ -7,12 +7,18 @@ ELEVEN_API_KEY = os.getenv("ELEVENLABS_API_KEY")
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
+camera_id = 0  # Change to the index of your camera
+
+# Replace this with the correct microphone index
+mic_index = 1  # Change to the index of your microphone
+
 VOICE_ID = "" # <------ Whatever Voice ID You Want!
 RECORD_SECONDS = 5
 FILENAME = "temp.wav"
 SAMPLERATE = 44100  # CD-quality audio
 
 SAMPLE_RATE = 16000
+CHANNEL = 1 # Change to your right channel
 FRAME_DURATION = 30  # ms
 FRAME_SIZE = int(SAMPLE_RATE * FRAME_DURATION / 1000)
 MAX_SILENCE_FRAMES = 15  # ~0.5 seconds of silence
