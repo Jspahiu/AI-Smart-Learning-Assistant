881887ae5fc3e9b297d3191bf9a5db163c2860dd
Jspahiu <121984809+Jspahiu@users.noreply.github.com>
2025-08-08 12:46:24 -0500

    Update my_face_recognition.py

--- Linux (Raspberry Pi 5)/my_face_recognition.py
+++ Linux (Raspberry Pi 5)/my_face_recognition.py
@@ -54,8 +54,6 @@ class MyFaceRecognition():
                 y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
                 if name != "Unknown":
-                    if "Jon" in name:
-                        name = "Jon Spahiu"
                     cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
 
