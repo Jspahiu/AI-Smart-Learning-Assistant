c7f27407703e2977052b45c990a632a8a4435945
Jspahiu <121984809+Jspahiu@users.noreply.github.com>
2025-08-19 08:56:56 -0500

    Update my_voice_and_video_chatbot.py

--- Linux (Raspberry Pi 5)/my_voice_and_video_chatbot.py
+++ Linux (Raspberry Pi 5)/my_voice_and_video_chatbot.py
@@ -14,23 +14,20 @@ threads = []
 detected_faces = DetectedFaces()
 stop_event = threading.Event()
 
-face_thread = MyFaceRecognition(camera_id, "MyWindow", "images", detected_faces, stop_event)
-face_thread.daemon = True
-face_thread.start()
-threads.append(face_thread)
-
-
 
 voice_thread = VoiceRecognitionThread(mic_index, detected_faces, stop_event)
 voice_thread.daemon = True
 voice_thread.start()
 threads.append(voice_thread)
 
+face_recognition = MyFaceRecognition(camera_id, "MyWindow", "images", detected_faces, stop_event)
+face_recognition.run()
+
+
+
 # Wait for all threads to finish
 for thread in threads:
     thread.join()
-
-
 
 
 
